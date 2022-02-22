72009592b5c99bcb4055761ccd61659c34a7ef89
waseem0abuameer <waseem.a.abuameer@gmail.com>
2022-02-22 08:29:10 +0200

    dsfsd

--- code-challenges/challenges-08.js
+++ code-challenges/challenges-08.js
@@ -24,14 +24,15 @@
 // }
 //
 // Output:
-// ["Customer Name :"+Romio Joliat+" , Age :"35", "Customer Name :Mario Ristrova , Age :39", ... ]
+// ["Customer Name :Romio Joliat , Age :35", "Customer Name :Mario Ristrova , Age :39", ... ]
 
 const customerAndAge = (obj) => {
-  for (const [key, value] of Object.entries(recipeInfo))  {
-    cos.push(`Customer Name:${key},Age: ${value}`);
-  
-      }
-  return cos;
+    let array = [];
+    for (let i in obj) {
+
+        array.push(`Customer Name :${i} , Age :${obj[i]}`);
+    }
+    return array;
 };
 // -------------------------------------------------------------------------------------------------------
 
@@ -57,12 +58,13 @@ const customerAndAge = (obj) => {
 // You can solve this challenge by using Object.entries
 
 const getEntries = (obj) => {
-  
-  for (const [key, value] of Object.entries(recipeInfo)) {
-    s.push(`${key}: ${value}`);
-  
-      }
-  return s;
+
+    let array = []
+    for (const [index, value] of Object.entries(obj)) {
+        array.push(`${index}: ${value}`);
+    }
+
+    return array;
 };
 // -------------------------------------------------------------------------------------------------------
 
@@ -74,39 +76,46 @@ const getEntries = (obj) => {
 // first one is containing the names of all of the courses in the data set.
 // second one is containing the names of all the students
 
-const courses = [
-  {
-    course: 'Java',
-    Instructor: 'David',
-    Students: ['Lincoln', 'Ruth', 'Briana', 'Suzy', 'Greta'],
-    GroupName: 'Stars',
-  },
-  {
-    course: 'JavaScript',
-    Instructor: 'Van',
-    Students: ['Alphonso', 'Daley', 'Dax', 'Karter', 'Jorja'],
-    GroupName: 'Nerd-ware',
-  },
-  {
-    course: 'Python',
-    Instructor: 'Delaney',
-    Students: ['Barney', 'Kalé', 'Alisha'],
-    GroupName: 'Whats-Up',
-  },
-  {
-    course: 'DotNet',
-    Instructor: 'Keanna',
-    Students: ['Oli', 'Gisselle', 'Pru'],
-    GroupName: 'Lol',
-  },
+const courses = [{
+        course: 'Java',
+        Instructor: 'David',
+        Students: ['Lincoln', 'Ruth', 'Briana', 'Suzy', 'Greta'],
+        GroupName: 'Stars',
+    },
+    {
+        course: 'JavaScript',
+        Instructor: 'Van',
+        Students: ['Alphonso', 'Daley', 'Dax', 'Karter', 'Jorja'],
+        GroupName: 'Nerd-ware',
+    },
+    {
+        course: 'Python',
+        Instructor: 'Delaney',
+        Students: ['Barney', 'Kalé', 'Alisha'],
+        GroupName: 'Whats-Up',
+    },
+    {
+        course: 'DotNet',
+        Instructor: 'Keanna',
+        Students: ['Oli', 'Gisselle', 'Pru'],
+        GroupName: 'Lol',
+    },
 ];
 
 const getInfo = (arr) => {
-  let coursesName = [];
-  let studentsName = [];
-  // write your code here
 
-  return { coursesName, studentsName };
+    let coursesName = [];
+    let studentsName = [];
+
+    for (let i of arr) {
+
+        coursesName.push(i.course);
+
+        for (let j of i.Students) {
+            studentsName.push(j);
+        }
+    }
+    return { coursesName, studentsName };
 };
 // -------------------------------------------------------------------------------------------------------
 
@@ -128,14 +137,37 @@ const getInfo = (arr) => {
 // ]
 
 const getStudents = (arr) => {
-  // write your code here
+
+    let objectArr = []
+
+    for (let i of arr) {
+
+        for (let j of courses) {
+
+            for (let z of j.Students) {
+
+                if (z == i) {
+
+                    let newObject = {
+                        Student: z,
+                        course: j.course,
+                    }
+
+                    objectArr.push(newObject);
+                    break;
+
+                }
+            }
+        }
+    }
+    return objectArr;
 };
 //  ------------------------------------------------------------------------------------------------------
 
 module.exports = {
-  customerAndAge,
-  getEntries,
-  courses,
-  getInfo,
-  getStudents,
+    customerAndAge,
+    getEntries,
+    courses,
+    getInfo,
+    getStudents,
 };
